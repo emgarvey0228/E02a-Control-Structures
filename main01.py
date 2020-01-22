47f816a28c24cc3a2dab5ff0d24bf751c6f10228
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-01-22 17:13:13 -0500

    unfinished

--- main01.py
+++ main01.py
@@ -6,3 +6,5 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 print('Greetings!')
 input("What is my favorite color? ")
+#I know that it's going to say "Greetings!" in the terminal and on the next line it is going to ask me my favorite color
+#After I ran python, it was confirmed that the words in the orange font were going to be asked in the terminal
--- main02.py
+++ main02.py
@@ -7,3 +7,4 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 print('Greetings!')
 color = input("What is my favorite color? ")
 print(color)
+#Main2 is different from Main1 because there is now 3 inputs instead of 1
