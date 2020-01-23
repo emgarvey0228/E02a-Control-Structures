685f7fed2976422e7a71d1f33a83b67425ae5dff
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-01-22 21:47:04 -0500

    added comments

--- main04.py
+++ main04.py
@@ -10,3 +10,6 @@ if (color == 'Red' or color == 'red'):
     print('Correct!')
 else:
     print('Sorry, try again.')
+    # This looks different from Main3 because now you have the input option of spelling Red like red
+    # This is going to help solve the issue of having the output tell the user that their input is incorrect when it is correct, just has a slightly different variable
+    # I spelled "rED" into the terminal and the response was "Sorry, try again."
