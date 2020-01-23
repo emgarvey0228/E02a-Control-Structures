a57ee621746d635b374620bd43ed011a8142d7da
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-01-22 21:41:25 -0500

    added comments

--- main02.py
+++ main02.py
@@ -7,4 +7,5 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 print('Greetings!')
 color = input("What is my favorite color? ")
 print(color)
-#Main2 is different from Main1 because there is now 3 inputs instead of 1
+# Main2 is different from Main1 because there is now 3 inputs instead of 1
+# I think the print (color) line is the response you make
--- main03.py
+++ main03.py
@@ -10,3 +10,8 @@ if (color == 'Red'):
     print('Correct!')
 else:
     print('Sorry, try again.')
+    # If the input color is red then the response would be correct
+    # Lines 9-12 is a guessing game 
+    # Lines 10 and 12 are indented because they represent a response
+    # If the R in red is not capitalized, then the guess would be incorrect and the response is "Sorry, try again."
+    # This tells me that color is tricky and if not spelled correctly or if it has a different capitalization, then the input would be incorrect
